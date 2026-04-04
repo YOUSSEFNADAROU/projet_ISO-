6985f75d7752a4cddfdfcdd1b938501593ecc4e7
hananeessah <119158650+hananeessah@users.noreply.github.com>
2026-04-04 01:17:58 +0100

    Mon travail sur ma-branche

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,14 +5,10 @@ import CompanyDashboard from './pages/company/CompanyDashboard';
 import AuditorLogin from './pages/auditor/AuditorLogin';
 import CompanySelector from './pages/auditor/CompanySelector';
 import Home from './pages/Home';
-import Scenario from './pages/Scenario';
 import Controls from './pages/Controls';
 import ControlDetail from './pages/ControlDetail';
 import Dashboard from './pages/Dashboard';
 import Report from './pages/Report';
-import Login from './pages/Login';
-import ProtectedRoute from './components/ProtectedRoute';
-import { AuthProvider } from './contexts/AuthContext';
 
 function App() {
   return (
@@ -24,69 +20,12 @@ function App() {
         <Route path="/auditor/login" element={<AuditorLogin />} />
         <Route path="/auditor/companies" element={<CompanySelector />} />
         <Route path="/home" element={<Home />} />
-        <Route path="/scenario" element={<Scenario />} />
         <Route path="/controls" element={<Controls />} />
         <Route path="/controls/:id" element={<ControlDetail />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/report" element={<Report />} />
       </Routes>
     </Router>
-    <AuthProvider>
-      <Router>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route
-            path="/"
-            element={
-              <ProtectedRoute>
-                <Home />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/scenario"
-            element={
-              <ProtectedRoute>
-                <Scenario />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/controls"
-            element={
-              <ProtectedRoute>
-                <Controls />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/controls/:id"
-            element={
-              <ProtectedRoute>
-                <ControlDetail />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/dashboard"
-            element={
-              <ProtectedRoute>
-                <Dashboard />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/report"
-            element={
-              <ProtectedRoute>
-                <Report />
-              </ProtectedRoute>
-            }
-          />
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
-      </Router>
-    </AuthProvider>
   );
 }
 
